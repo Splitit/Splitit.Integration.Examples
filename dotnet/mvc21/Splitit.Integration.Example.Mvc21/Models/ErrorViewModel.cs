b6f00b2ff0a363c40ac617e6c3dd3fd64b8a347e
Ivan Cesar <icesar@tvz.hr>
2020-03-11 18:42:15 +0100

    Basic example for .NET MVC 2.1 with upstream messaging.

---
+++ dotnet/mvc21/Splitit.Integration.Example.Mvc21/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Splitit.Integration.Example.Mvc21.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
