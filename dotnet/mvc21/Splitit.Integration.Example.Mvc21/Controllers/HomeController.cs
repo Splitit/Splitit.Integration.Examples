607ccf9d637be90a5e4c71f784474405c64bcafc
Ivan Cesar <icesar@tvz.hr>
2020-03-19 14:34:14 +0100

    Removed splitit.sdk completely.

--- dotnet/mvc21/Splitit.Integration.Example.Mvc21/Controllers/HomeController.cs
+++ dotnet/mvc21/Splitit.Integration.Example.Mvc21/Controllers/HomeController.cs
@@ -11,9 +11,9 @@ using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using Splitit.Integration.Example.Mvc21.Models;
-using Splitit.SDK.Client.Api;
-using Splitit.SDK.Client.Client;
-using Splitit.SDK.Client.Model;
+//using Splitit.SDK.Client.Api;
+//using Splitit.SDK.Client.Client;
+//using Splitit.SDK.Client.Model;
 
 namespace Splitit.Integration.Example.Mvc21.Controllers
 {
@@ -47,23 +47,10 @@ namespace Splitit.Integration.Example.Mvc21.Controllers
         [HttpPost]
         public async Task<JsonResult> FlexFieldsExample(decimal amount, int? numInstallments = null)
         {
-            LoginResponse loginResult = null;
+            //LoginResponse loginResult = null;
 
             try
             {
-                var billingAddress = new AddressData();
-                var consumerModel = new ConsumerData(isLocked: false, isDataRestricted: false);
-
-                await this.TryUpdateModelAsync(billingAddress, "billingAddress");
-                await this.TryUpdateModelAsync(consumerModel, "consumerModel");
-
-                Configuration.Sandbox.AddApiKey(this._configuration["SplititApiKey"]);
-
-                //var loginApi = new LoginApi(Configuration.Sandbox);
-                //var request = new LoginRequest(userName: this._configuration["SplititApiUsername"], password: this._configuration["SplititApiPassword"]);
-
-                //loginResult = await loginApi.LoginPostAsync(request);
-
                 var logger = new LoggingHandler(new HttpClientHandler());
 
                 using (var client = new HttpClient(logger))
@@ -79,87 +66,103 @@ namespace Splitit.Integration.Example.Mvc21.Controllers
                     var data = new StringContent(json, Encoding.UTF8, "application/json");
                     var result = await client.PostAsync("/api/Login", data);
 
-                    var manualResponse = await result.Content.ReadAsAsync<LoginResponse>();
+                    //var manualResponse = await result.Content.ReadAsAsync<LoginResponse>();
                     return Json(new { logger.Log });
                 }
 
-                var initRequest = new InitiateInstallmentPlanRequest()
-                {
-                    PlanData = new PlanData(
-                        amount: new MoneyWithCurrencyCode(amount, "USD"),
-                        numberOfInstallments: numInstallments,
-                        attempt3DSecure: false),
-                    BillingAddress = billingAddress,
-                    ConsumerData = consumerModel,
-                    PaymentWizardData = new PaymentWizardData(
-                        requestedNumberOfInstallments: "1,2,4,6,8",
-                        isOpenedInIframe: false),
-                    RedirectUrls = new RedirectUrls(
-                        succeeded: "https://www.success.com/",
-                        failed: "https://www.ynet.co.il/",
-                        canceled: "https://www.walla.com/")
-                };
 
-                using (var client = new HttpClient())
-                {
-                    client.BaseAddress = new Uri("https://webapi.sandbox.splitit.com/");
+                //var billingAddress = new AddressData();
+                //var consumerModel = new ConsumerData(isLocked: false, isDataRestricted: false);
 
-                    var json = JsonConvert.SerializeObject(new
-                    {
-                        PlanData = new PlanData(
-                            amount: new MoneyWithCurrencyCode(amount, "USD"),
-                            numberOfInstallments: numInstallments,
-                            attempt3DSecure: false),
-                        BillingAddress = billingAddress,
-                        ConsumerData = consumerModel,
-                        PaymentWizardData = new PaymentWizardData(
-                            requestedNumberOfInstallments: "1,2,4,6,8",
-                            isOpenedInIframe: false),
-                        RedirectUrls = new RedirectUrls(
-                            succeeded: "https://www.success.com/",
-                            failed: "https://www.ynet.co.il/",
-                            canceled: "https://www.walla.com/"),
-                        RequestHeader = new
-                        {
-                            ApiKey = this._configuration["SplititApiKey"],
-                            SessionId = loginResult.SessionId
-                        }
-                    });
+                //await this.TryUpdateModelAsync(billingAddress, "billingAddress");
+                //await this.TryUpdateModelAsync(consumerModel, "consumerModel");
 
-                    var data = new StringContent(json, Encoding.UTF8, "application/json");
-                    var result = await client.PostAsync("/api/InstallmentPlan/Initiate", data);
+                //Configuration.Sandbox.AddApiKey(this._configuration["SplititApiKey"]);
 
-                    var manualResponse = await result.Content.ReadAsStringAsync();
-                    return Json(new { loginResult, manual = manualResponse, result.Headers, result.StatusCode });
+                ////var loginApi = new LoginApi(Configuration.Sandbox);
+                ////var request = new LoginRequest(userName: this._configuration["SplititApiUsername"], password: this._configuration["SplititApiPassword"]);
 
-                }
+                ////loginResult = await loginApi.LoginPostAsync(request);
 
-                var installmentPlanApi = new InstallmentPlanApi(Configuration.Sandbox, sessionId: loginResult.SessionId);
-                var initResponse = installmentPlanApi.InstallmentPlanInitiate(new InitiateInstallmentPlanRequest()
-                {
-                    PlanData = new PlanData(
-                            amount: new MoneyWithCurrencyCode(amount, "USD"),
-                            numberOfInstallments: numInstallments,
-                            attempt3DSecure: false),
-                    BillingAddress = billingAddress,
-                    ConsumerData = consumerModel,
-                    PaymentWizardData = new PaymentWizardData(
-                            requestedNumberOfInstallments: "1,2,4,6,8",
-                            isOpenedInIframe: false),
-                    RedirectUrls = new RedirectUrls(
-                            succeeded: "https://www.success.com/",
-                            failed: "https://www.ynet.co.il/",
-                            canceled: "https://www.walla.com/")
-                });
+                
 
-                return new JsonResult(initResponse, new Newtonsoft.Json.JsonSerializerSettings()
-                {
-                    ContractResolver = new CamelCasePropertyNamesContractResolver()
-                });
+                //var initRequest = new InitiateInstallmentPlanRequest()
+                //{
+                //    PlanData = new PlanData(
+                //        amount: new MoneyWithCurrencyCode(amount, "USD"),
+                //        numberOfInstallments: numInstallments,
+                //        attempt3DSecure: false),
+                //    BillingAddress = billingAddress,
+                //    ConsumerData = consumerModel,
+                //    PaymentWizardData = new PaymentWizardData(
+                //        requestedNumberOfInstallments: "1,2,4,6,8",
+                //        isOpenedInIframe: false),
+                //    RedirectUrls = new RedirectUrls(
+                //        succeeded: "https://www.success.com/",
+                //        failed: "https://www.ynet.co.il/",
+                //        canceled: "https://www.walla.com/")
+                //};
+
+                //using (var client = new HttpClient())
+                //{
+                //    client.BaseAddress = new Uri("https://webapi.sandbox.splitit.com/");
+
+                //    var json = JsonConvert.SerializeObject(new
+                //    {
+                //        PlanData = new PlanData(
+                //            amount: new MoneyWithCurrencyCode(amount, "USD"),
+                //            numberOfInstallments: numInstallments,
+                //            attempt3DSecure: false),
+                //        BillingAddress = billingAddress,
+                //        ConsumerData = consumerModel,
+                //        PaymentWizardData = new PaymentWizardData(
+                //            requestedNumberOfInstallments: "1,2,4,6,8",
+                //            isOpenedInIframe: false),
+                //        RedirectUrls = new RedirectUrls(
+                //            succeeded: "https://www.success.com/",
+                //            failed: "https://www.ynet.co.il/",
+                //            canceled: "https://www.walla.com/"),
+                //        RequestHeader = new
+                //        {
+                //            ApiKey = this._configuration["SplititApiKey"],
+                //            SessionId = loginResult.SessionId
+                //        }
+                //    });
+
+                //    var data = new StringContent(json, Encoding.UTF8, "application/json");
+                //    var result = await client.PostAsync("/api/InstallmentPlan/Initiate", data);
+
+                //    var manualResponse = await result.Content.ReadAsStringAsync();
+                //    return Json(new { loginResult, manual = manualResponse, result.Headers, result.StatusCode });
+
+                //}
+
+                //var installmentPlanApi = new InstallmentPlanApi(Configuration.Sandbox, sessionId: loginResult.SessionId);
+                //var initResponse = installmentPlanApi.InstallmentPlanInitiate(new InitiateInstallmentPlanRequest()
+                //{
+                //    PlanData = new PlanData(
+                //            amount: new MoneyWithCurrencyCode(amount, "USD"),
+                //            numberOfInstallments: numInstallments,
+                //            attempt3DSecure: false),
+                //    BillingAddress = billingAddress,
+                //    ConsumerData = consumerModel,
+                //    PaymentWizardData = new PaymentWizardData(
+                //            requestedNumberOfInstallments: "1,2,4,6,8",
+                //            isOpenedInIframe: false),
+                //    RedirectUrls = new RedirectUrls(
+                //            succeeded: "https://www.success.com/",
+                //            failed: "https://www.ynet.co.il/",
+                //            canceled: "https://www.walla.com/")
+                //});
+
+                //return new JsonResult(initResponse, new Newtonsoft.Json.JsonSerializerSettings()
+                //{
+                //    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                //});
             }
             catch(Exception ex)
             {
-                return Json(new { exception = ex, loginData = loginResult });
+                return Json(new { exception = ex, loginData = "" });
             }
             
         }
