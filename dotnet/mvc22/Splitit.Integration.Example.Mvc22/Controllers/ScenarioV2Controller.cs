904a58145d1bcae9e9eab827e270d06098dfff27
Ivan Cesar <ivan.cesar@splitit.com>
2021-04-21 10:17:09 +0200

    Auto align feature for v2

--- dotnet/mvc22/Splitit.Integration.Example.Mvc22/Controllers/ScenarioV2Controller.cs
+++ dotnet/mvc22/Splitit.Integration.Example.Mvc22/Controllers/ScenarioV2Controller.cs
@@ -276,5 +276,16 @@ namespace Splitit.Integration.Example.Mvc21.Controllers
                 Picker = picker
             });
         }
+
+        public IActionResult AutoAlign(int options = 5, decimal amount = 500, string currency = "USD", string culture = "en-US")
+        {
+            return View(new CommonTestModel()
+            {
+                PublicToken = FlexFields.Authenticate(this.FlexFieldsEnv, SplititApiUsername, SplititApiPassword)
+                    .AddInstallments(Enumerable.Range(1, options).ToList())
+                    .GetPublicToken(amount, currency),
+                Culture = culture
+            });
+        }
     }
 }
